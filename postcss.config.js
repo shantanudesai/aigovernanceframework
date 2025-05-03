52fc760e829fcba5bf7b861abd975468e140da67
shantanudesai <shantanudesai1@gmail.com>
2025-05-03 18:36:47 +0530

    Update dependencies and configuration files, fix framework data structure

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {}
+    'tailwindcss': {},
+    'autoprefixer': {},
+    'postcss-import': {},
+    'postcss-nesting': {}
   }
 } 
