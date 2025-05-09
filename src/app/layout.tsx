b042f0d8909b878a09bb15a32a4efb9419b30b3e
shantanudesai <shantanudesai1@gmail.com>
2025-05-09 23:33:59 +0530

    feat: add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './globals.css';
 import { Inter, Poppins } from 'next/font/google';
 import Footer from '@/components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 const poppins = Poppins({
@@ -25,6 +26,7 @@ export default function RootLayout({
       <body className="min-h-screen bg-zinc-50 flex flex-col">
         <main className="flex-grow">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
